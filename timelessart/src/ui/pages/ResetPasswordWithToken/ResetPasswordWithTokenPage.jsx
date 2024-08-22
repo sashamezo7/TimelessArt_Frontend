1ea3291e696b5ce2205d84c4c90c7e37c5467a31
angheluta ruxandra <anghelutaruxandra88@gmail.com>
2024-08-22 17:20:35 +0300

    ceva

--- timelessart/src/ui/pages/ResetPasswordWithToken/ResetPasswordWithTokenPage.jsx
+++ timelessart/src/ui/pages/ResetPasswordWithToken/ResetPasswordWithTokenPage.jsx
@@ -2,7 +2,7 @@ import { useTranslation } from 'react-i18next';
 import Navbar from '../../components/Navbar/Navbar.jsx';
 import InputField from '../components/InputField/InputField.jsx'
 import passwordIcon from '../../../assets/password.svg'
-import ResetPasswordWithTokenComponent from 'C:/Users/anghe/timelessArt1/frontend/TimelessArt_Frontend/timelessart/src/ui/components/Navbar/ResetPasswordForm.jsx';
+import ResetPasswordWithTokenComponent from '../../components/Navbar/ResetPasswordForm.jsx';
 
 
 import { useState } from 'react';
